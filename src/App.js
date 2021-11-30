34574bf62e4087cc091bf3093a237dd37f25d0d2
Cornerstone <fortunecornerstone@gmail.com>
2021-11-30 12:30:52 +0100

    fixed routing issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { Stack } from '@chakra-ui/layout'
-import React from 'react'
-import CheckoutPage from './components/CheckoutPage'
+import { Stack } from "@chakra-ui/layout";
+import React from "react";
+import CheckoutPage from "./components/CheckoutPage";
+import ShippingMethod from "./components/ShippingMethod";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Stack bg='#E5E5E5' h='100%' w='100vw' maxHeight='100%'>
-      <CheckoutPage />
+    <Stack bg="#E5E5E5" h="100%" w="100vw" maxHeight="100%">
+      {/* <CheckoutPage /> */}
+      <Router>
+        <Switch>
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route exact path="/shipping" component={ShippingMethod} />
+        </Switch>
+      </Router>
     </Stack>
   );
 }
 
-export default App
+export default App;
